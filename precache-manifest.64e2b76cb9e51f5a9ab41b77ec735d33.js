3424baaba431ad708b6146f140ea41abe9e35054
kajol <36220862+kajolrauniyar@users.noreply.github.com>
2020-07-23 18:04:48 +0530

    Updates

---
+++ precache-manifest.64e2b76cb9e51f5a9ab41b77ec735d33.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639dc70e52eb418396a2128890a10bd8",
+    "url": "/Protofolio-using-React/index.html"
+  },
+  {
+    "revision": "00793afb64ff4a8818af",
+    "url": "/Protofolio-using-React/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4797922137c054f627e9",
+    "url": "/Protofolio-using-React/static/css/main.6e113a80.chunk.css"
+  },
+  {
+    "revision": "00793afb64ff4a8818af",
+    "url": "/Protofolio-using-React/static/js/2.01e8a7e1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Protofolio-using-React/static/js/2.01e8a7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4797922137c054f627e9",
+    "url": "/Protofolio-using-React/static/js/main.f57b7347.chunk.js"
+  },
+  {
+    "revision": "e8a20b18d474beccce8a",
+    "url": "/Protofolio-using-React/static/js/runtime-main.63c1cd16.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/Protofolio-using-React/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/Protofolio-using-React/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/Protofolio-using-React/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/Protofolio-using-React/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Protofolio-using-React/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/Protofolio-using-React/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/Protofolio-using-React/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/Protofolio-using-React/static/media/software.43049cae.jpeg"
+  }
+]);
